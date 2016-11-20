9a7f4d059f4728b40801c4ca6792c073ff85b6dc
Konstantin Murugov <komurugov@gmail.com>
2016-11-20 23:19:41 +0300

    Test Case Results class in progress

--- shotmaker/TestCase.cs
+++ shotmaker/TestCase.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace shotmaker
 {
@@ -26,6 +27,25 @@ namespace shotmaker
         public string Title { get; private set; }
         public List<string> Preconditions { get; private set; }
         public List<Verification> Verifications { get; private set; }
+
+        static TestCase LoadFromXml(XmlDocument doc) { return null; }
+    }
+
+    abstract class Screenshotable
+    {
+        public 
+    }
+
+    class TestCaseResults
+    {
+        public TestCase TestCase { get; private set; }
+
+        public enum TResult { Passed, Failed }
+        public enum TStatus { None, Done, Skipped }
+
+        public bool SetPrecondition(bool shot, int precondition) { return false; }
+        public bool SetData(bool shot, int verification, int data) { return false; }
+        public bool SetResult(bool shot, int verification, int step, int subStep, TResult result) { return false; }
     }
 
 }
