5269b62308148a3b2a2d8911e063c76d116a8629
Konstantin Murugov <komurugov@gmail.com>
2016-11-20 12:44:41 +0300

    Structure of Test Case (script)

---
+++ shotmaker/TestCase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shotmaker
+{
+    class StepAndExpectedResults
+    {
+        public string Step { get; private set; }
+        public List<string> ExpectedResults { get; private set; }
+    }
+
+    class Verification
+    {
+        public List<string> Data { get; private set; }
+        public List<StepAndExpectedResults> StepAndExpectedResults { get; private set; }
+    }
+
+    class TestCase
+    {
+        public string ExecutionID { get; private set; }
+        public string ExecutionTitle { get; private set; }
+        public string ID { get; private set; }
+        public string Title { get; private set; }
+        public List<string> Preconditions { get; private set; }
+        public List<Verification> Verifications { get; private set; }
+    }
+
+}
